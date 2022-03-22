c28c75456a83403e354030c8b05cc8b95aafecef
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-03-22 15:05:02 +0000

    Added edited

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Stella S
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
+
+  function doMathClicked () {
+  // this function does basic math
+  document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
+  document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
+
+}
+
